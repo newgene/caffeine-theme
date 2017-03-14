9cf43f8f7e907103f13b429e2ce9556df5b4e933
kevinxin90 <kevinxin@scripps.edu>
2017-03-14 11:28:20 -0700

    test search rss

--- assets/js/src/search.js
+++ assets/js/src/search.js
@@ -1,4 +1,42 @@
 "use strict";
+function json2xml(o, tab) {
+   var toXml = function(v, name, ind) {
+      var xml = "";
+      if (v instanceof Array) {
+         for (var i=0, n=v.length; i<n; i++)
+            xml += ind + toXml(v[i], name, ind+"\t") + "\n";
+      }
+      else if (typeof(v) == "object") {
+         var hasChild = false;
+         xml += ind + "<" + name;
+         for (var m in v) {
+            if (m.charAt(0) == "@")
+               xml += " " + m.substr(1) + "=\"" + v[m].toString() + "\"";
+            else
+               hasChild = true;
+         }
+         xml += hasChild ? ">" : "/>";
+         if (hasChild) {
+            for (var m in v) {
+               if (m == "#text")
+                  xml += v[m];
+               else if (m == "#cdata")
+                  xml += "<![CDATA[" + v[m] + "]]>";
+               else if (m.charAt(0) != "@")
+                  xml += toXml(v[m], m, ind+"\t");
+            }
+            xml += (xml.charAt(xml.length-1)=="\n"?ind:"") + "</" + name + ">";
+         }
+      }
+      else {
+         xml += ind + "<" + name + ">" + v.toString() +  "</" + name + ">";
+      }
+      return xml;
+   }, xml="";
+   for (var m in o)
+      xml += toXml(o[m], m, "");
+   return tab ? xml.replace(/\t/g, tab) : xml.replace(/\t|\n/g, "");
+}
 
 $(function() {
     var $searchField = $("#search-field"),
@@ -19,7 +57,7 @@ $(function() {
       url:'https://api.rss2json.com/v1/api.json?rss_url=http%3A%2F%2Fmygene.info%2Frss%2F',
       dataType: 'jsonp', // Notice! JSONP <-- P (lowercase)
       success:function(json){
-        rss_doc = json;
+        rss_doc = json2xml(json);
         console.log(rss_doc);
       },
       error:function(){
